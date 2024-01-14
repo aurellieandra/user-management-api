14b793bdc9d3ab0e325a340ccba1702c071536ae
aurellieandra <diandra.azra@gmail.com>
2024-01-14 22:13:05 +0700

    adding crud role user + register

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,15 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
-exports.module = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
   },
+
   "test": {
     "username": "root",
     "password": null,
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import express, { Request, Response } from "express";
 
 import dotenv from "dotenv";
+import router from "./routes/Routes";
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
     return res.status(200).send({
-        success: true,
+        status: 200,
         message: "Success GET route",
     });
 });
+
+app.use(router);
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on PORT ${process.env.PORT}`);
