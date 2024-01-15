bc29e51d992449614adf49c82b39fc4b5d45b8ed
aurellieandra <diandra.azra@gmail.com>
2024-01-15 21:14:26 +0700

    finalizing refresh token feature

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,308 +1,323 @@
-import { Request, Response } from 'express';
-import User from '../db/models/User';
-import Helper from '../helpers/Helper';
-import PasswordHelper from '../helpers/PasswordHelper';
+import { Request, Response } from "express";
+import User from "../db/models/User";
+import Helper from "../helpers/Helper";
+import PasswordHelper from "../helpers/PasswordHelper";
 
 const register = async (req: Request, res: Response): Promise<Response> => {
-	try {
-		const { name, email, password, confirmPassword } = req.body;
+    try {
+        const { name, email, password, confirmPassword } = req.body;
 
-		const hashed = await PasswordHelper.PasswordHashing(password);
+        const hashed = await PasswordHelper.PasswordHashing(password);
 
-		const user = await User.create({
-			name,
-			email,
-			password: hashed,
-			active: true,
-			verified: true,
-			roleID: 1,
-		});
+        const user = await User.create({
+            name,
+            email,
+            password: hashed,
+            active: true,
+            verified: true,
+            roleID: 1,
+        });
 
-		return res
-			.status(200)
-			.send(Helper.ResponseData(201, 'Successfully create user', null, user));
-	} catch (error: any) {
-		return res.status(500).send(Helper.ResponseData(500, '', error, null));
-	}
+        return res
+            .status(200)
+            .send(
+                Helper.ResponseData(201, "Successfully create user", null, user)
+            );
+    } catch (error: any) {
+        return res.status(500).send(Helper.ResponseData(500, "", error, null));
+    }
 };
 
 const login = async (req: Request, res: Response): Promise<Response> => {
-	try {
-		const { email, password } = req.body;
-		const user = await User.findOne({
-			where: {
-				email: email,
-			},
-		});
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({
+            where: {
+                email: email,
+            },
+        });
 
-		if (!user) {
-			return res
-				.status(401)
-				.send(Helper.ResponseData(401, 'Unauthorized Access', null, null));
-		}
+        if (!user) {
+            return res
+                .status(401)
+                .send(
+                    Helper.ResponseData(401, "Unauthorized Access", null, null)
+                );
+        }
 
-		const matched = await PasswordHelper.PasswordCompare(
-			password,
-			user.password
-		);
-		if (!matched) {
-			return res
-				.status(401)
-				.send(Helper.ResponseData(401, 'Unauthorized Access', null, null));
-		}
+        const matched = await PasswordHelper.PasswordCompare(
+            password,
+            user.password
+        );
+        if (!matched) {
+            return res
+                .status(401)
+                .send(
+                    Helper.ResponseData(401, "Unauthorized Access", null, null)
+                );
+        }
 
-		const dataUser = {
-			name: user.name,
-			email: user.email,
-			roleID: user.roleID,
-			verified: user.verified,
-			active: user.active,
-		};
+        const dataUser = {
+            name: user.name,
+            email: user.email,
+            roleID: user.roleID,
+            verified: user.verified,
+            active: user.active,
+        };
 
-		const token = Helper.GenerateToken(dataUser);
-		const refreshToken = Helper.GenerateRefreshToken(dataUser);
+        const token = Helper.GenerateToken(dataUser);
+        const refreshToken = Helper.GenerateRefreshToken(dataUser);
 
-		(user.accessToken = refreshToken), await user.save();
+        (user.accessToken = refreshToken), await user.save();
 
-		res.cookie('refreshToken', refreshToken, {
-			httpOnly: true,
-			maxAge: 24 * 60 * 60 * 1000,
-		});
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000,
+        });
 
-		const responseUser = {
-			name: user.name,
-			email: user.email,
-			roleID: user.roleID,
-			verified: user.verified,
-			active: user.active,
-			token,
-		};
+        const responseUser = {
+            name: user.name,
+            email: user.email,
+            roleID: user.roleID,
+            verified: user.verified,
+            active: user.active,
+            token,
+        };
 
-		return res
-			.status(200)
-			.send(
-				Helper.ResponseData(200, 'Successfully logged in', null, responseUser)
-			);
-	} catch (error) {
-		return res.status(500).send(Helper.ResponseData(500, '', error, null));
-	}
+        return res
+            .status(200)
+            .send(
+                Helper.ResponseData(
+                    200,
+                    "Successfully logged in",
+                    null,
+                    responseUser
+                )
+            );
+    } catch (error) {
+        return res.status(500).send(Helper.ResponseData(500, "", error, null));
+    }
 };
 
 const refreshToken = async (req: Request, res: Response): Promise<Response> => {
-	try {
-		const refreshToken = req.cookies.refreshToken;
-		console.log(req.cookies)
-		if (!refreshToken) {
-			return res.status(401).send(Helper.ResponseData(401, "Unauthorized", null, null));
-		}
+    try {
+        const refreshToken = req.cookies?.refreshToken;
+        if (!refreshToken) {
+            return res
+                .status(401)
+                .send(Helper.ResponseData(401, "Unauthorized", null, null));
+        }
 
-		const decodedUser = Helper.ExtractRefreshToken(refreshToken);
-		console.log(decodedUser);
-		if (!decodedUser) {
-			return res.status(401).send(Helper.ResponseData(401, "Unauthorized", null, null));
-		}
+        const decodedUser = Helper.ExtractRefreshToken(refreshToken);
+        if (!decodedUser) {
+            return res
+                .status(401)
+                .send(Helper.ResponseData(401, "Unauthorized", null, null));
+        }
 
-		const token = Helper.GenerateToken({
-			name: decodedUser.name,
-			email: decodedUser.email,
-			roleID: decodedUser.roleID,
-			verified: decodedUser.verified,
-			active: decodedUser.active
-		});
+        const token = Helper.GenerateToken({
+            name: decodedUser.name,
+            email: decodedUser.email,
+            roleID: decodedUser.roleID,
+            verified: decodedUser.verified,
+            active: decodedUser.active,
+        });
 
-		const resultUser = {
-			name: decodedUser.name,
-			email: decodedUser.email,
-			roleID: decodedUser.roleID,
-			verified: decodedUser.verified,
-			active: decodedUser.active,
-			token: token
-		}
+        const resultUser = {
+            name: decodedUser.name,
+            email: decodedUser.email,
+            roleID: decodedUser.roleID,
+            verified: decodedUser.verified,
+            active: decodedUser.active,
+            token: token,
+        };
 
-		return res.status(200).send(Helper.ResponseData(200, "OK", null, resultUser));
-	} catch (error) {
-		return res.status(500).send(Helper.ResponseData(500, '', error, null));
-	}
+        return res
+            .status(200)
+            .send(Helper.ResponseData(200, "OK", null, resultUser));
+    } catch (error) {
+        return res.status(500).send(Helper.ResponseData(500, "", error, null));
+    }
 };
 
 const getUsers = async (req: Request, res: Response): Promise<Response> => {
-	try {
-		const users = await User.findAll({
-			where: {
-				active: true,
-			},
-		});
+    try {
+        const users = await User.findAll({
+            where: {
+                active: true,
+            },
+        });
 
-		return res.status(200).send({
-			status: 200,
-			message: 'Successfully Get Users',
-			data: users,
-		});
-	} catch (error: any) {
-		if (error != null && error instanceof Error) {
-			return res.status(500).send({
-				status: 500,
-				message: error.message,
-				errors: error,
-			});
-		}
+        return res.status(200).send({
+            status: 200,
+            message: "Successfully Get Users",
+            data: users,
+        });
+    } catch (error: any) {
+        if (error != null && error instanceof Error) {
+            return res.status(500).send({
+                status: 500,
+                message: error.message,
+                errors: error,
+            });
+        }
 
-		return res.status(500).send({
-			status: 500,
-			message: 'Internal Server Error',
-			errors: error,
-		});
-	}
+        return res.status(500).send({
+            status: 500,
+            message: "Internal Server Error",
+            errors: error,
+        });
+    }
 };
 
 const getRoleByID = async (req: Request, res: Response): Promise<Response> => {
-	try {
-		const { id } = req.params;
-		const role = await User.findByPk(id);
+    try {
+        const { id } = req.params;
+        const role = await User.findByPk(id);
 
-		if (!role) {
-			return res.status(404).send({
-				status: 404,
-				message: 'Data Not Found',
-				data: null,
-			});
-		}
+        if (!role) {
+            return res.status(404).send({
+                status: 404,
+                message: "Data Not Found",
+                data: null,
+            });
+        }
 
-		return res.status(200).send({
-			status: 200,
-			message: 'Successfully Get User Role by ID',
-			data: role,
-		});
-	} catch (error: any) {
-		if (error != null && error instanceof Error) {
-			return res.status(500).send({
-				status: 500,
-				message: error.message,
-				errors: error,
-			});
-		}
+        return res.status(200).send({
+            status: 200,
+            message: "Successfully Get User Role by ID",
+            data: role,
+        });
+    } catch (error: any) {
+        if (error != null && error instanceof Error) {
+            return res.status(500).send({
+                status: 500,
+                message: error.message,
+                errors: error,
+            });
+        }
 
-		return res.status(500).send({
-			status: 500,
-			message: 'Internal Server Error',
-			errors: error,
-		});
-	}
+        return res.status(500).send({
+            status: 500,
+            message: "Internal Server Error",
+            errors: error,
+        });
+    }
 };
 
 const createRole = async (req: Request, res: Response): Promise<Response> => {
-	try {
-		const { roleName, active } = req.body;
+    try {
+        const { roleName, active } = req.body;
 
-		const role = await User.create({
-			active,
-		});
+        const role = await User.create({
+            active,
+        });
 
-		return res.status(201).send({
-			status: 201,
-			message: 'Successfully Create User Role',
-			data: role,
-		});
-	} catch (error: any) {
-		if (error != null && error instanceof Error) {
-			return res.status(500).send({
-				status: 500,
-				message: error.message,
-				errors: error,
-			});
-		}
+        return res.status(201).send({
+            status: 201,
+            message: "Successfully Create User Role",
+            data: role,
+        });
+    } catch (error: any) {
+        if (error != null && error instanceof Error) {
+            return res.status(500).send({
+                status: 500,
+                message: error.message,
+                errors: error,
+            });
+        }
 
-		return res.status(500).send({
-			status: 500,
-			message: 'Internal Server Error',
-			errors: error,
-		});
-	}
+        return res.status(500).send({
+            status: 500,
+            message: "Internal Server Error",
+            errors: error,
+        });
+    }
 };
 
 const updateRole = async (req: Request, res: Response): Promise<Response> => {
-	try {
-		const { id } = req.params;
-		const { roleName, active } = req.body;
+    try {
+        const { id } = req.params;
+        const { roleName, active } = req.body;
 
-		const role = await User.findByPk(id);
+        const role = await User.findByPk(id);
 
-		if (!role) {
-			return res.status(404).send({
-				status: 404,
-				message: 'Data Not Found',
-				data: null,
-			});
-		}
+        if (!role) {
+            return res.status(404).send({
+                status: 404,
+                message: "Data Not Found",
+                data: null,
+            });
+        }
 
-		role.active = active;
-		await role.save();
+        role.active = active;
+        await role.save();
 
-		return res.status(201).send({
-			status: 201,
-			message: 'Successfully Update User Role',
-			data: role,
-		});
-	} catch (error: any) {
-		if (error != null && error instanceof Error) {
-			return res.status(500).send({
-				status: 500,
-				message: error.message,
-				errors: error,
-			});
-		}
+        return res.status(201).send({
+            status: 201,
+            message: "Successfully Update User Role",
+            data: role,
+        });
+    } catch (error: any) {
+        if (error != null && error instanceof Error) {
+            return res.status(500).send({
+                status: 500,
+                message: error.message,
+                errors: error,
+            });
+        }
 
-		return res.status(500).send({
-			status: 500,
-			message: 'Internal Server Error',
-			errors: error,
-		});
-	}
+        return res.status(500).send({
+            status: 500,
+            message: "Internal Server Error",
+            errors: error,
+        });
+    }
 };
 
 const deleteRole = async (req: Request, res: Response): Promise<Response> => {
-	try {
-		const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-		const role = await User.findByPk(id);
+        const role = await User.findByPk(id);
 
-		if (!role) {
-			return res.status(404).send({
-				status: 404,
-				message: 'Data Not Found',
-				data: null,
-			});
-		}
+        if (!role) {
+            return res.status(404).send({
+                status: 404,
+                message: "Data Not Found",
+                data: null,
+            });
+        }
 
-		await role.destroy();
+        await role.destroy();
 
-		return res.status(201).send({
-			status: 201,
-			message: 'Successfully Delete User Role',
-			data: null,
-		});
-	} catch (error: any) {
-		if (error != null && error instanceof Error) {
-			return res.status(500).send({
-				status: 500,
-				message: error.message,
-				errors: error,
-			});
-		}
+        return res.status(201).send({
+            status: 201,
+            message: "Successfully Delete User Role",
+            data: null,
+        });
+    } catch (error: any) {
+        if (error != null && error instanceof Error) {
+            return res.status(500).send({
+                status: 500,
+                message: error.message,
+                errors: error,
+            });
+        }
 
-		return res.status(500).send({
-			status: 500,
-			message: 'Internal Server Error',
-			errors: error,
-		});
-	}
+        return res.status(500).send({
+            status: 500,
+            message: "Internal Server Error",
+            errors: error,
+        });
+    }
 };
 
 export default {
-	register,
-	login,
-	refreshToken,
-	/**
+    register,
+    login,
+    refreshToken,
+    /**
     getRoles,
     getRoleByID,
     createRole,
